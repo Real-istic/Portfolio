23001adc70b6e8e2da38710a2397bf2ea01aa97b
Real-istic <RealSkaarj@gmx.de>
2023-06-18 18:51:55 +0200

    grounded design and above the fold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboveTheFoldComponent
   ],
   imports: [
     BrowserModule,
