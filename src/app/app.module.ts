453cc2b2e21ee33f44b1452c1c07e929a4de9fad
Real-istic <RealSkaarj@gmx.de>
2023-06-20 20:25:40 +0200

    lots of css adjustments to all components + a lot widescreen responsiveness

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AboveTheFoldComponent
+    AboveTheFoldComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
