215e9b32b0b8f580f731c415fdba07f56b677750
Real-istic <RealSkaarj@gmx.de>
2023-06-23 20:09:16 +0200

    contact form integrated - validation still in progress

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -8,4 +8,47 @@ import { Component } from '@angular/core';
 
 export class ContactComponent {
 
+  @ViewChild('myForm') myForm: any;
+  @ViewChild('nameField') nameField: any;
+  @ViewChild('messageField') messageField: any;
+  @ViewChild('emailField') emailField: any;
+  @ViewChild('sendButton') sendButton: any;
+
+
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  async sendMail() {
+    let nameField = this.nameField.nativeElement;
+    let emailField = this.emailField.nativeElement;
+    let messageField = this.messageField.nativeElement;
+    let sendButton = this.sendButton.nativeElement;
+
+    nameField.disabled = true;
+    emailField.disabled = true;
+    messageField.disabled = true;
+    sendButton.disabled = true;
+
+    // animation
+
+    let fd = new FormData();
+    fd.append('name', nameField.value);
+    fd.append('email', emailField.value);
+    fd.append('message', messageField.value);
+
+    //send the form data to the server
+    await fetch('https://patrizio-marzullo.developerakademie.net/portfolio/send_mail.php', {
+      method: 'POST',
+      body: fd
+    });
+
+    // show message: Message sent
+
+    nameField.disabled = false;
+    emailField.disabled = false;
+    messageField.disabled = false;
+    sendButton.disabled = false;
+  }
 }
