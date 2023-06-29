10668335c55720e543e51f94b7f6568554e69197
Real-istic <RealSkaarj@gmx.de>
2023-06-29 12:17:06 +0200

    responsive navbar menu done

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  @ViewChild('respMenu') respMenu: any;
+  @ViewChild('menuRight') menuRight: any;
+  @ViewChild('bar1') bar1: any;
+  @ViewChild('bar2') bar2: any;
+  @ViewChild('bar3') bar3: any;
+
+  toggleResponsiveMenu() {
+    let menuRight = this.menuRight.nativeElement;
+    menuRight.classList.toggle('hide');
+    this.toggleResponsiveMenuIcon();
+  }
+
+  toggleResponsiveMenuIcon() {
+    let bar1 = this.bar1.nativeElement;
+    let bar2 = this.bar2.nativeElement;
+    let bar3 = this.bar3.nativeElement;
+    bar1.classList.toggle('rotate-45deg');
+    bar2.classList.toggle('width-zero');
+    bar3.classList.toggle('rotate--45deg');
+  }
 }
