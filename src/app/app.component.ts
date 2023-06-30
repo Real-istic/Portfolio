4b9d3a1e176b88fa1409369cbb247fc1ababc0b8
Real-istic <RealSkaarj@gmx.de>
2023-07-01 01:46:39 +0200

    scroll effects installed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from "@angular/router";
-
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +10,8 @@ import { Router } from "@angular/router";
 export class AppComponent {
   title = 'portfolio';
   constructor(public router: Router) { }
+
+  ngOnInit() {
+     AOS.init();
+  }
 }
